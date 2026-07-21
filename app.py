f4a57b044a50c9ea0cf528a724d5950b933c535d
ADRIEL SEBASTIAN JOSEPH <108691488+adrielsebastian17@users.noreply.github.com>
2026-07-21 13:26:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ TEAM_MEMBERS = ["Alan", "Jaden", "Wei Seng", "Kenny", "Luthfi", "Adriel"]
 if "machinery_df" not in st.session_state:
     data = {
         "Maintanence": ["GIC", "Google", "UBS", "Dulwich", "One George Street"],
-        "Location / Zone": ["L49,47,43,42,40,34,32,31", "BLK X LY", "XXX", "XXX", "ditorium"],
+        "Location / Zone": ["L49,47,43,42,40,34,32,31", "BLK X LY", "XXX", "XXX", "XXX"],
         "Relay Temp (°C)": [42.0, 68.5, 38.0, 71.2, 50.0],  # Over 65°C requires urgent attention
         "Network Ping (ms)": [15, 142, 12, 185, 25],       # Fixed values: High ping (>100) indicates network strain
         "Assigned To": ["Adriel", "Jaden,Kenny", "Jaden", "Wei Seng", "Adriel"]
